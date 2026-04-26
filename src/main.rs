69364a5c4315e8c8669815555c95f47763703110
Martin Hagström <marhag87@gmail.com>
2026-04-26 10:21:20 +0200

    Use listener

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use device_query::{DeviceQuery, DeviceState, Keycode};
+use device_query::{DeviceEvents, DeviceEventsHandler, DeviceQuery, DeviceState, Keycode};
 use enigo::{Button, Direction, Enigo, Mouse, Settings};
+use std::error::Error;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 use std::time::Duration;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let is_clicking = Arc::new(AtomicBool::new(false));
     let is_clicking_clone = Arc::clone(&is_clicking);
 
@@ -24,18 +25,15 @@ fn main() {
         }
     });
 
-    let device_state = DeviceState::new();
-    let mut was_f8_pressed = false;
-
-    loop {
-        let keys = device_state.get_keys();
-        let is_f8_pressed = keys.contains(&Keycode::F8);
-        if is_f8_pressed && !was_f8_pressed {
+    let device_events = DeviceEventsHandler::new(Duration::from_millis(10))
+        .ok_or("Failed to start device listener")?;
+    let _guard = device_events.on_key_down(move |key| {
+        if *key == Keycode::F8 {
             let current_state = is_clicking.load(Ordering::Relaxed);
             is_clicking.store(!current_state, Ordering::Relaxed);
             println!("Clicker Active: {}", !current_state);
         }
-        was_f8_pressed = is_f8_pressed;
-        thread::sleep(Duration::from_millis(10));
-    }
+    });
+    thread::park();
+    Ok(())
 }
